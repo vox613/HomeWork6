18e8ef83cd2f065377025faf33bddaff1acf9b81
Alexey <vox613@gmail.com>
2019-11-14 01:24:39 +0300

    First commit!

---
+++ src/main/java/innopolis/homework6/Main.java
@@ -0,0 +1,11 @@
+package innopolis.homework6;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
