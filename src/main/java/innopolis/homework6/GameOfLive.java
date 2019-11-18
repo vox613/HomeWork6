8619500e076c4cd37fe702603a71c9fd1393c445
Alexey <vox613@gmail.com>
2019-11-18 16:29:42 +0300

    Add JavaDocs.

--- src/main/java/innopolis/homework6/GameOfLive.java
+++ src/main/java/innopolis/homework6/GameOfLive.java
@@ -9,6 +9,13 @@ import java.util.Random;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
+/**
+ * The main class of the "Game of Life" program. Implemented one and multi-threaded version of the game.
+ * The program accepts the initial state of the field from the file as an input or randomly generates a field
+ * of the specified size. Then the specified number of iterations of the field calculation is carried out.
+ * After that, the final configuration of the field and the time spent on single and multi-threaded calculations
+ * are recorded in the specified output file.
+ */
 public class GameOfLive implements Runnable {
 
     private final int ITERATIONS_NUM;
@@ -18,11 +25,20 @@ public class GameOfLive implements Runnable {
     private byte[][] lifeGeneration;
     private byte[][] nextGeneration;
     private byte[][] tempo;
-    private final boolean fileWrite;
     private final boolean randomFieldGenerate;
     private final String outputFile;
 
-
+    /**
+     * The class constructor takes as arguments the number of iterations of the calculation, the size of the field and
+     * the path to the file for recording the result.
+     *
+     * @param ITERATIONS_NUM - number of iterations of field recalculation. The number is greater than 0,
+     *                       otherwise the default number of iterations will be set to 10.
+     * @param FIELD_SIZE     - edge size of the field. An integer greater than 3, otherwise the default size
+     *                       will be set to 10.
+     * @param outputFile     - file path for recording calculation results. If the file is missing, it will be created.
+     *                       Each time the file is overwritten.
+     */
     GameOfLive(int ITERATIONS_NUM, int FIELD_SIZE, String outputFile) {
         this.ITERATIONS_NUM = (ITERATIONS_NUM > 0) ? ITERATIONS_NUM : 10;
         this.FIELD_SIZE = (FIELD_SIZE > 3) ? FIELD_SIZE : 10;
@@ -35,16 +51,24 @@ public class GameOfLive implements Runnable {
         this.lifeGeneration = new byte[FIELD_SIZE][FIELD_SIZE];
         this.nextGeneration = new byte[FIELD_SIZE][FIELD_SIZE];
         this.tempo = new byte[FIELD_SIZE][FIELD_SIZE];
-        this.fileWrite = true;
         this.randomFieldGenerate = true;
         this.outputFile = outputFile;
     }
 
-
-    public GameOfLive(String filePath, String outputFile, int iterations, boolean fileWrite) {
+    /**
+     * The class constructor takes as arguments the path to the file with the initial configuration of the field,
+     * the path to the file to record the result, as well as the number of iterations of the calculation.
+     *
+     * @param filePath       - The path to the file with the initial field configuration. Missing a file will trigger
+     *                       an IOException.
+     * @param outputFile     - file path for recording calculation results. If the file is missing, it will be created.
+     *                       Each time the file is overwritten.
+     * @param ITERATIONS_NUM - number of iterations of field recalculation. The number is greater than 0,
+     *                       otherwise the default number of iterations will be set to 10.
+     */
+    public GameOfLive(String filePath, String outputFile, String ITERATIONS_NUM) {
         readFile(filePath);
-        this.fileWrite = fileWrite;
-        this.ITERATIONS_NUM = (iterations > 0) ? iterations : 10;
+        this.ITERATIONS_NUM = (Integer.parseInt(ITERATIONS_NUM) > 0) ? Integer.parseInt(ITERATIONS_NUM) : 10;
 
         this.FIELD_SIZE = tempo.length;
         this.counter = 0;
@@ -60,138 +84,19 @@ public class GameOfLive implements Runnable {
         this.outputFile = outputFile;
     }
 
-
-    private void readFile(String path) {
-        try (FileInputStream inStream = new FileInputStream(path)) {
-            byte[] localMass = new byte[inStream.available()];
-            inStream.read(localMass, 0, inStream.available());
-            inStream.close();
-            parseFile(localMass);
-        } catch (IOException ex) {
-            System.out.println(ex);
-        }
-    }
-
-
-    private void parseFile(byte[] localMass) {
-        int numElements = 0;
-        int count = 0;
-        for (int i = 0; i < localMass.length; i++) {
-            if ((localMass[i] == 48) || (localMass[i] == 49)) {
-                localMass[count] = (localMass[i] == 48) ? (byte) 0 : (byte) 1;
-                numElements++;
-                count++;
-            }
-            if (localMass[i] == (byte) 10) {
-                numElements = 0;
-            }
-        }
-        tempo = new byte[numElements][numElements];
-        for (int i = 0; i < numElements * numElements; i++) {
-            tempo[i / numElements][i % numElements] = localMass[i];
-        }
-        for (int i = 0; i < numElements; i++) {
-            System.out.println(Arrays.toString(tempo[i]));
-        }
-    }
-
-
     @Override
     public void run() {
         fillRandomField();
         byteArrayCopy(lifeGeneration, tempo);
         singleThreadCompute();
-        reset();
+        resetCounterAndFields();
         multiThreadCompute();
-
-    }
-
-
-    private void writeResultToFile(boolean appendFile, String str) {
-        try (BufferedWriter bufferWriter = new BufferedWriter(new FileWriter(outputFile, appendFile))) {
-            for (int i = 0; i < FIELD_SIZE; i++) {
-                for (int j = 0; j < FIELD_SIZE; j++) {
-                    bufferWriter.write(lifeGeneration[i][j] + "  ");
-                }
-                bufferWriter.write("\n");
-            }
-            bufferWriter.write(str);
-            bufferWriter.write("\n");
-        } catch (IOException e) {
-            System.out.println(e);
-        }
     }
 
 
     /**
-     *
-     */
-    private class CountNeighbors {
-        /**
-         * @param x
-         * @param y
-         */
-        private void generate(int x, int y) {
-            int count = countNeighbors(x, y);
-            nextGeneration[x][y] = lifeGeneration[x][y];
-            nextGeneration[x][y] = ((count == 3) || (nextGeneration[x][y] == 1)) ? (byte) 1 : (byte) 0;
-            nextGeneration[x][y] = (((count >= 2) && (count <= 3)) && (nextGeneration[x][y] == 1)) ? (byte) 1 : (byte) 0;
-        }
-
-        /**
-         * @param x
-         * @param y
-         * @return
-         */
-        private int countNeighbors(int x, int y) {
-            int count = 0;
-            for (int dx = -1; dx < 2; dx++) {
-                for (int dy = -1; dy < 2; dy++) {
-                    int nX = x + dx;
-                    int nY = y + dy;
-                    nX = (nX < 0) ? FIELD_SIZE - 1 : nX;
-                    nY = (nY < 0) ? FIELD_SIZE - 1 : nY;
-                    nX = (nX > FIELD_SIZE - 1) ? 0 : nX;
-                    nY = (nY > FIELD_SIZE - 1) ? 0 : nY;
-                    count += (lifeGeneration[nX][nY] == 1) ? 1 : 0;
-                }
-            }
-            if (lifeGeneration[x][y] == 1) {
-                count--;
-            }
-            return count;
-        }
-    }
-
-    /**
-     *
-     */
-    private class InnerClass {
-        private int x1, y1, x2, y2;
-
-        InnerClass(int x1, int y1, int x2, int y2) {
-            this.x1 = x1;
-            this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2;
-        }
-
-        /**
-         *
-         */
-        private void processOfLife() {
-            CountNeighbors computing = new CountNeighbors();
-            for (int x = x1; x < x2; x++) {
-                for (int y = y1; y < y2; y++) {
-                    computing.generate(x, y);
-                }
-            }
-        }
-    }
-
-
-    /**
-     *
+     * Multithreaded calculation of the state of the field. The original field is divided into 4 parts and
+     * each is processed by a separate thread.
      */
     private void multiThreadCompute() {
         long currentTime = System.currentTimeMillis();
@@ -208,13 +113,13 @@ public class GameOfLive implements Runnable {
             counter++;
         });
 
-        InnerClass inn1 = new InnerClass(fieldSplitIndex[0][0], fieldSplitIndex[0][1],
+        ExecuteClass inn1 = new ExecuteClass(fieldSplitIndex[0][0], fieldSplitIndex[0][1],
                 fieldSplitIndex[0][2], fieldSplitIndex[0][3]);
-        InnerClass inn2 = new InnerClass(fieldSplitIndex[1][0], fieldSplitIndex[1][1],
+        ExecuteClass inn2 = new ExecuteClass(fieldSplitIndex[1][0], fieldSplitIndex[1][1],
                 fieldSplitIndex[1][2], fieldSplitIndex[1][3]);
-        InnerClass inn3 = new InnerClass(fieldSplitIndex[2][0], fieldSplitIndex[2][1],
+        ExecuteClass inn3 = new ExecuteClass(fieldSplitIndex[2][0], fieldSplitIndex[2][1],
                 fieldSplitIndex[2][2], fieldSplitIndex[2][3]);
-        InnerClass inn4 = new InnerClass(fieldSplitIndex[3][0], fieldSplitIndex[3][1],
+        ExecuteClass inn4 = new ExecuteClass(fieldSplitIndex[3][0], fieldSplitIndex[3][1],
                 fieldSplitIndex[3][2], fieldSplitIndex[3][3]);
 
         Thread thread1 = new Thread(() -> {
@@ -292,17 +197,13 @@ public class GameOfLive implements Runnable {
 
 
     /**
-     *
+     * Single-threaded calculation of the state of the field.
      */
     private void singleThreadCompute() {
         long currentTime = System.currentTimeMillis();
-        CountNeighbors computing = new CountNeighbors();
+        ExecuteClass singleThreadExecute = new ExecuteClass(0, 0, FIELD_SIZE, FIELD_SIZE);
         while (counter < ITERATIONS_NUM) {
-            for (int x = 0; x < FIELD_SIZE; x++) {
-                for (int y = 0; y < FIELD_SIZE; y++) {
-                    computing.generate(x, y);
-                }
-            }
+            singleThreadExecute.processOfLife();
             for (int x = 0; x < FIELD_SIZE; x++) {
                 System.arraycopy(nextGeneration[x], 0, lifeGeneration[x], 0, FIELD_SIZE);
             }
@@ -312,6 +213,77 @@ public class GameOfLive implements Runnable {
         String timeCompute = "singleThreadCompute time = " + (System.currentTimeMillis() - currentTime);
         writeResultToFile(false, timeCompute);
         System.out.println(timeCompute);
+    }
+
+
+    /**
+     * A class containing general methods for calculating a single and multi-threaded game algorithm.
+     */
+    private class CountNeighbors {
+        /**
+         * The method determines the state of the current cell based on information about its neighbors.
+         *
+         * @param x - cell coordinate in a two-dimensional array along the x axis
+         * @param y - cell coordinate in a two-dimensional array along the y axis
+         */
+        private void generate(int x, int y) {
+            int count = countNeighbors(x, y);
+            nextGeneration[x][y] = lifeGeneration[x][y];
+            nextGeneration[x][y] = ((count == 3) || (nextGeneration[x][y] == 1)) ? (byte) 1 : (byte) 0;
+            nextGeneration[x][y] = (((count >= 2) && (count <= 3)) && (nextGeneration[x][y] == 1)) ? (byte) 1 : (byte) 0;
+        }
+
+        /**
+         * Calculates the number of living neighbors of the transferred cell.
+         *
+         * @param x  - cell coordinate in a two-dimensional array along the x axis
+         * @param y- cell coordinate in a two-dimensional array along the y axis
+         * @return - number of living neighbors
+         */
+        private int countNeighbors(int x, int y) {
+            int count = 0;
+            for (int dx = -1; dx < 2; dx++) {
+                for (int dy = -1; dy < 2; dy++) {
+                    int nX = x + dx;
+                    int nY = y + dy;
+                    nX = (nX < 0) ? FIELD_SIZE - 1 : nX;
+                    nY = (nY < 0) ? FIELD_SIZE - 1 : nY;
+                    nX = (nX > FIELD_SIZE - 1) ? 0 : nX;
+                    nY = (nY > FIELD_SIZE - 1) ? 0 : nY;
+                    count += (lifeGeneration[nX][nY] == 1) ? 1 : 0;
+                }
+            }
+            if (lifeGeneration[x][y] == 1) {
+                count--;
+            }
+            return count;
+        }
+    }
+
+    /**
+     * The class recalculates the field according to the specified coordinates.
+     */
+    private class ExecuteClass {
+        private final int x1, y1, x2, y2;
+
+        ExecuteClass(int x1, int y1, int x2, int y2) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+
+        /**
+         * The method recalculates the field according to the specified coordinates.
+         */
+        private void processOfLife() {
+            CountNeighbors computing = new CountNeighbors();
+            for (int x = x1; x < x2; x++) {
+                for (int y = y1; y < y2; y++) {
+                    computing.generate(x, y);
+                }
+            }
+        }
     }
 
 
@@ -335,15 +307,61 @@ public class GameOfLive implements Runnable {
         }
     }
 
-    /**
-     *
-     */
-    private void reset() {
+    private void writeResultToFile(boolean appendFile, String str) {
+        try (BufferedWriter bufferWriter = new BufferedWriter(new FileWriter(outputFile, appendFile))) {
+            for (int i = 0; i < FIELD_SIZE; i++) {
+                for (int j = 0; j < FIELD_SIZE; j++) {
+                    bufferWriter.write(lifeGeneration[i][j] + "  ");
+                }
+                bufferWriter.write("\n");
+            }
+            bufferWriter.write(str);
+            bufferWriter.write("\n");
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    private void readFile(String path) {
+        try (FileInputStream inStream = new FileInputStream(path)) {
+            byte[] localMass = new byte[inStream.available()];
+            inStream.read(localMass, 0, inStream.available());
+            inStream.close();
+            parseFile(localMass);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+
+
+    private void parseFile(byte[] localMass) {
+        int numElements = 0;
+        int count = 0;
+        for (int i = 0; i < localMass.length; i++) {
+            if ((localMass[i] == 48) || (localMass[i] == 49)) {
+                localMass[count] = (localMass[i] == 48) ? (byte) 0 : (byte) 1;
+                numElements++;
+                count++;
+            }
+            if (localMass[i] == (byte) 10) {
+                numElements = 0;
+            }
+        }
+        tempo = new byte[numElements][numElements];
+        for (int i = 0; i < numElements * numElements; i++) {
+            tempo[i / numElements][i % numElements] = localMass[i];
+        }
+        for (int i = 0; i < numElements; i++) {
+            System.out.println(Arrays.toString(tempo[i]));
+        }
+    }
+
+
+    private void resetCounterAndFields() {
         counter = 0;
         byteArrayCopy(tempo, lifeGeneration);
         nextGeneration = new byte[FIELD_SIZE][FIELD_SIZE];
     }
-
 
     private void byteArrayCopy(byte[][] from, byte[][] to) {
         for (int x = 0; x < FIELD_SIZE; x++) {
--- src/main/java/innopolis/homework6/Main.java
+++ src/main/java/innopolis/homework6/Main.java
@@ -3,10 +3,25 @@ package innopolis.homework6;
 
 import java.io.IOException;
 
+/**
+ * The main class of the program from which the launch of the "Game of Life".
+ */
 public class Main {
+    /**
+     * The main method of the program from which the launch of the "Game of Life" is carried out.
+     *
+     * @param args - Array of command line arguments.
+     *             args [0] - the path to the file with the input data, the initial state of the playing field,
+     *                        for example, ".\\resources\\input"
+     *             args [1] - the path to the file with the output data, the final state of the playing field, and the
+     *                        execution time of the single-threaded and multi-threaded implementation,
+     *                        for example, ".\\resources\\output".
+     *             args [2] - the number of iterations of the calculation.
+     * @throws IOException - throws when reading a file fails or the file is missing.
+     */
     public static void main(String[] args) throws IOException {
         //GameOfLive game = new GameOfLive(1000, 50, args[1]);
-        GameOfLive game = new GameOfLive(args[0], args[1], Integer.parseInt(args[2]), Boolean.parseBoolean(args[3]));
+        GameOfLive game = new GameOfLive(args[0], args[1], args[2]);
         new Thread(game).start();
     }
 }
